c1a3f686cbebad180196369512ac69151582abbd
Shuvamdt <shuvamdt1230@gmail.com>
2025-06-29 00:10:23 +0530

    Hero Section fix

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
           ref={box2}
           className="h-15 w-15 bg-[#03071E] absolute hidden sm:flex sm:left-125 md:left-50 top-50"
         ></div>
-        <h1 className="font-1 text-l mx-5 sm:my-5 sm:py-5 sm:text-6xl lg:text-7xl md:text-end z-1">
+        <h1 className="font-1 w-70 sm:w-auto text-l mx-5 sm:my-5 sm:py-5 text-5xl sm:text-6xl lg:text-7xl md:text-end z-1">
           Manage Your Money,
           <br />{" "}
           <span className="text-[#D00000] font-italic">Effortlessly</span>
@@ -56,18 +56,18 @@ const Hero = () => {
           manage your earnings and savings.
         </p>
         <div className="flex flex-col justify-center items-end">
-          <div className="h-6 sm:h-15 lg:h-20 w-175 md:w-180 absolute flex justify-center items-end top-5 sm:top-16 lg:top-14 overflow-hidden">
+          <div className="h-25 sm:h-15 lg:h-20 w-175 md:w-180 absolute flex justify-center items-end top-5 sm:top-16 lg:top-14 overflow-hidden">
             <div
               ref={box3}
-              className="h-6 sm:h-15 lg:h-20 w-175 md:w-180 bg-[#D00000] absolute flex justify-center items-end top-0 sm:top-0 left-180"
+              className="h-25 sm:h-15 lg:h-20 w-175 md:w-180 bg-[#D00000] absolute flex justify-center items-end top-0 sm:top-0 left-180"
             ></div>
           </div>
         </div>
         <div className="flex flex-col justify-center items-end">
-          <div className="h-6 sm:h-15 lg:h-20 w-150 md:w-100 absolute flex justify-center items-end top-11 sm:top-30.5 md:top-31 lg:top-34 right-26 sm:right-70 md:right-4 overflow-x-hidden">
+          <div className="h-15 sm:h-15 lg:h-20 w-150 md:w-100 absolute flex justify-center items-end top-29 sm:top-30.5 md:top-31 lg:top-34 right-26 sm:right-70 md:right-4 overflow-x-hidden">
             <div
               ref={box4}
-              className="h-6 sm:h-15 lg:h-20 w-150 md:w-90 bg-[#03071E] absolute flex justify-center items-end top-0 sm:top-0 left-150 md:left-150"
+              className="h-15 sm:h-15 lg:h-20 w-150 md:w-90 bg-[#03071E] absolute flex justify-center items-end top-0 sm:top-0 left-150 md:left-150"
             ></div>
           </div>
         </div>
